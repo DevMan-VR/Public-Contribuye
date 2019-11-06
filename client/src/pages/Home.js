97fe21afcae94dbfc1fe124f606621b682942608
SebastianV <sebastian.villegas@alumnos.uv>
2019-11-05 22:45:46 -0300

    Añadida característica Card a Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,88 +13,64 @@ const Home = () => (
 
         {/** End Carousel */}
         {/** Section Category */}
-        <div className="row">
-        
-            <div className="container p-0" >
-            <hr/>
 
             <div className="row mb-3">
                 <h2 style={{color:'gray', margin: 'auto'}}>Categorias</h2>
             </div>
-            <hr/>
-                <div className="row w-100">
-                    <div className={"col-3 p-0"}>
+
+                    <div className="grid-container">
                         <CategoryDiv
                             title="Salud"
                             category="health"
                             itemList={["Terapia Psicologica","Consulta Nutricional","Más..."]}
 
                         />
-                    </div>
-                    <div className="col-3 p-0">
+
                         <CategoryDiv
                             title="Leyes"
                             category="law"
                             itemList={["Apoyo Legal", "Derechos Humanos", "Apoyo Denuncias y Más..."]}
                         />
-                    </div>
-                    <div className="col-3 p-0">
+
                         <CategoryDiv
                             title="Tecnologia"
                             category="tech"
                             itemList={["Paginas web de abastecimiento", "Aplicaciones móviles para la democracia", "Más..."]}
                         />
-                    </div>
-                    <div className="col-3 p-0">
+               
+
                     <CategoryDiv
                         title="Aprendizaje"
                         category="learning"
                         itemList={["Cursos", "Talleres","Seminarios y Más..."]}
                     />
-                    </div>
+   
 
-                </div>
-                <div className="row w-100">
-                    <div className="col-3 p-0">
+
+
+
                     <CategoryDiv
                         title="Eventos"
                         category="events"
                         itemList={["Culturales","Sociales","Más..."]}
                     />
-                    </div>
-                    <div className="col-3 p-0">
                     <CategoryDiv
                         title="Negocios Locales"
                         category="local-market"
                         itemList={["Comida","Productos","Servicios Rebajados y más..."]}
                     />
-                    </div>
-                    <div className="col-3 p-0">
                     <CategoryDiv
                         title="Ofertas y Rebajas"
                         category="offers"
                         itemList={["Productos"]}
                     />
-                    </div>
-                    <div className="col-3 p-0">
                     <CategoryDiv
                         title="Otros"
                         category="others"
                         itemList={["Fotografia","Yoga","Deportes y más ..."]}
                     />
-                    </div>
-
                 </div>
-
             </div>
-        </div>
-        
-
-        
-        {/** End Section Category */}
-    </div>
-
-
 )
 
 export default Home;
